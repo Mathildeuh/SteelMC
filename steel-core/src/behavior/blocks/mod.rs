edef2cb5e6e582d3f98aa3056976c43e21c1fa64
JunkyDeveloper <junkydeveloper@gmail.com>
2026-09-02 22:03:12 +0200

    rename and remove more things

--- steel-core/src/behavior/blocks/mod.rs
+++ steel-core/src/behavior/blocks/mod.rs
@@ -67,7 +67,7 @@ pub use vegetation::{
     BaseCoralFanBlock, BaseCoralPlantBlock, BaseCoralWallFanBlock, BigDripleafBlock,
     BigDripleafStemBlock, BushBlock, CarpetBlock, CaveVinesBlock, CaveVinesPlantBlock,
     ChorusFlowerBlock, ChorusPlantBlock, CoralFanBlock, CoralPlantBlock, CoralWallFanBlock,
-    DirtPathBlock, DryVegetationBlock, EyeblossomBlock, EyeblossomType, FarmlandBlock,
+    PathBlock, DryVegetationBlock, EyeblossomBlock, EyeblossomType, FarmlandBlock,
     FireflyBushBlock, FlowerBedBlock, GlowLichenBlock, HangingMossBlock, HangingRootsBlock,
     HugeMushroomBlock, KelpBlock, KelpPlantBlock, LeafLitterBlock, LilyPadBlock,
     MangrovePropaguleBlock, MangroveRootsBlock, MossyCarpetBlock, MushroomBlock, NetherFungusBlock,
